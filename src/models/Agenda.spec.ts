63da82ab24e197b139ebbafda9beaf4c98fe093b
Victor Martinez <knoopx@gmail.com>
2022-02-02 11:37:52 +0100

    fix: agenda spec

--- src/models/Agenda.spec.ts
+++ src/models/Agenda.spec.ts
@@ -1,9 +1,14 @@
+import { DateTime, Settings } from "luxon";
 import { expect, test } from "vitest";
 import { Store, Task } from ".";
+
+Settings.defaultZone = "Europe/Madrid";
+const Now = DateTime.local(2020, 1, 1);
+Settings.now = () => Now.toMillis();
 
 test("Agenda", () => {
   const store = Store.create();
   const task = Task.create({ expression: "task every monday" });
   store.addTask(task);
-  expect(store.agenda.groupEntries).toMatchObject([["tomorrow", [task]]]);
+  expect(store.agenda.groupEntries).toMatchObject([["next week", [task]]]);
 });
